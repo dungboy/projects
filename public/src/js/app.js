22eff4f1861158af610707397790daaaee33fa9f
이선협 <dungboy@coupang.com>
2016-07-18 16:28:33 +0900

    first commit

---
+++ public/src/js/app.js
@@ -0,0 +1,8 @@
+/**
+ * Created by coupang on 2016. 7. 18..
+ */
+var app = {
+	doSomething: function() {
+		alert('what am i?');
+	}
+};
---
+++ public/src/js/sample.js
@@ -0,0 +1,15 @@
+/**
+ * Created by coupang on 2016. 7. 18..
+ */
+(function() {
+	// do something...
+	var whatArray = [
+		'string 1',
+		'string 2',
+		'string 3'
+	];
+	function doSome() {
+		// do something
+	}
+	console.log(whatArray);
+})();
